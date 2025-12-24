6993074dfa2f670d211649a860dc37f7216f5232
HelalOmar2001 <halahelooo14@gmail.com>
2025-12-24 11:15:35 +0200

    init

---
+++ services/customer/src/main/java/com/helal/ecommerce/customer/Customer.java
@@ -0,0 +1,23 @@
+package com.helal.ecommerce.customer;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.annotation.processing.Generated;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Customer {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Address address;
+}
