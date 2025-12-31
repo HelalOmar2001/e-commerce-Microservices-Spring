934fa5bca72cb9cc5325926e921ca9d34dee0912
HelalOmar2001 <halahelooo14@gmail.com>
2025-12-31 14:13:45 +0200

    finalizing notification microservice

---
+++ services/notification/src/main/java/com/helal/ecommerce/notification/Notification.java
@@ -0,0 +1,24 @@
+package com.helal.ecommerce.notification;
+
+import com.helal.ecommerce.kafka.order.OrderConfirmation;
+import com.helal.ecommerce.kafka.payment.PaymentConfirmation;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Document
+public class Notification {
+    @Id
+    private String id;
+    private NotificationType type;
+    private LocalDateTime notificationDate;
+    private OrderConfirmation orderConfirmation;
+    private PaymentConfirmation paymentConfirmation;
+}
---
+++ services/notification/src/main/java/com/helal/ecommerce/notification/NotificationType.java
@@ -0,0 +1,6 @@
+package com.helal.ecommerce.notification;
+
+public enum NotificationType {
+    ORDER_CONFIRMATON,
+    PAYMENT_CONFIRMATON
+}
